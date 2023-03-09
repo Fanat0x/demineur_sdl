a02d4aed0004ac1131811356faab7c43490bcb4f
lorenzo-molina <116020466+lorenzo-molina@users.noreply.github.com>
2023-03-09 11:57:45 +0100

    grille finie

--- demineur_sdl/Main.c
+++ demineur_sdl/Main.c
@@ -1,26 +1,78 @@
 #include "stdio.h"
 #include "SDL.h"
 #undef main
+#include "stdbool.h"
 
-typedef int bool;
-#define true 1;
-#define false 0;
-
-//don't do this, this is just an example
 void update();
 void render(SDL_Renderer* renderer, SDL_Window* window, int tableau[10][10]);
 void displayGrid(SDL_Renderer* renderer, SDL_Window* window, int tableau[10][10]);
 
+void render(SDL_Renderer* renderer, SDL_Window* window, int tableau[10][10])
+{
+	SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
+	displayGrid(renderer, window, tableau);
+}
 
-//please don't put all your code in main like I did.
-int main() {
+void update()
+{
+
+}
+
+void displayGrid(SDL_Renderer* renderer, SDL_Window* window, int tableau[10][10])
+{
+	SDL_RenderClear(renderer);
+	SDL_Surface* imageCase = NULL;
+	SDL_Texture* texture = NULL;
+	SDL_Rect rectangle;
+
+	rectangle.x = 0;
+	rectangle.y = 0;
+
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		rectangle.y = 50 * i;
+		for (j = 0; j < 10; j++)
+		{
+			if ((i + j) % 2 == 0)
+			{
+				imageCase = SDL_LoadBMP("img/grass.bmp");
+			}
+			else
+			{
+				imageCase = SDL_LoadBMP("img/grass2.bmp");
+			}
+			rectangle.x = 50 * j;
+			texture = SDL_CreateTextureFromSurface(renderer, imageCase);
+			SDL_FreeSurface(imageCase);
+			SDL_QueryTexture(texture, NULL, NULL, &rectangle.h, &rectangle.w);
+			
+			SDL_RenderCopy(renderer, texture, NULL, &rectangle);
+			SDL_DestroyTexture(texture);
+		}
+		
+	}
+
+	SDL_RenderPresent(renderer);
+	SDL_DestroyTexture(texture);
+}
+
+int main()
+{
 
 	int tableau[10][10];
 	SDL_Renderer* renderer;
 	SDL_Window* window;
-	SDL_Rect rectangleTest;
+	SDL_Rect rectangle;
 	bool isRunning = true;
 	bool fullscreen;
+
+	float deltaTime = 0;
+	int time = 0;
+	int FPSTarget = 60;
+	float deltaTimeTarget = 1000 / (float)FPSTarget;
 
 	fullscreen = false;
 	int flags = 0;
@@ -28,8 +80,8 @@ int main() {
 	if (fullscreen) {
 		flags = flags | SDL_WINDOW_FULLSCREEN;
 	}
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) 
-		printf( "Subsystems Initialized!\n");
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+		printf("Subsystems Initialized!\n");
 
 	window = SDL_CreateWindow("Demineur", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, flags);
 	if (window) {
@@ -44,7 +96,7 @@ int main() {
 		isRunning = true;
 	}
 
-	
+
 
 	while (isRunning) {
 		SDL_Event event;
@@ -57,65 +109,22 @@ int main() {
 				break;
 			}
 		}
+		deltaTime = SDL_GetTicks() - time;
+
+		if (deltaTime < deltaTimeTarget) {
+			SDL_Delay(deltaTimeTarget - deltaTime);
+			deltaTime = deltaTimeTarget;
+		}
+		else
+			system("cls");
+			printf("%f\n", deltaTime);
 		update();
 		render(renderer, window, tableau);
 	}
 
-	//frees memory associated with renderer and window
 	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);	//error here
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
-
 	return 0;
-}
-
-
-//simple render function
-void render(SDL_Renderer* renderer, SDL_Window* window, int tableau[10][10]) {
-	SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
-	
-
-	//your stuff to render would typically go here.
-	displayGrid(renderer, window, tableau);
-	
-}
-
-//simple update function
-void update() {
-	//if things could update the code would go in here.
-}
-
-void displayGrid(SDL_Renderer* renderer, SDL_Window* window, int tableau[10][10])
-{
-	SDL_RenderClear(renderer);
-	SDL_Surface* imageCase = NULL;
-	SDL_Texture* texture = NULL;
-	SDL_Rect rectangleTest;
-
-	imageCase = SDL_LoadBMP("img/grass.bmp");
-
-	texture = SDL_CreateTextureFromSurface(renderer, imageCase);
-	
-	SDL_FreeSurface(imageCase);
-
-	SDL_QueryTexture(texture, NULL, NULL, &rectangleTest.h, &rectangleTest.w);
-
-	rectangleTest.x = 0;
-	rectangleTest.y = 0;
-
-	int i = 0;
-	int j = 0;
-
-	for (i = 0; i < 10; i++)
-	{
-		rectangleTest.y = 50 * i;
-		for (j = 0; j < 10; j++)
-		{
-			rectangleTest.x = 50 * j;
-			SDL_RenderCopy(renderer, texture, NULL, &rectangleTest);
-		}
-	}
-	SDL_RenderPresent(renderer);
-	SDL_DestroyTexture(texture);
 }
